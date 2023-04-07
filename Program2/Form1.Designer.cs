15a469d2c8b0e0a3e171449a5b445b679987b6b0
Jaelyn Gray <jjgray02@louisville.edu>
2023-04-06 23:02:59 -0400

    Add project files.

---
+++ Program2/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Program2
+{
+    partial class calculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.peopleLbl = new System.Windows.Forms.Label();
+            this.distanceLbl = new System.Windows.Forms.Label();
+            this.daysLbl = new System.Windows.Forms.Label();
+            this.companyALbl = new System.Windows.Forms.Label();
+            this.companyAOutputLbl = new System.Windows.Forms.Label();
+            this.companyBOutputLbl = new System.Windows.Forms.Label();
+            this.companyBLbl = new System.Windows.Forms.Label();
+            this.companyCOutputLbl = new System.Windows.Forms.Label();
+            this.companyCLbl = new System.Windows.Forms.Label();
+            this.peopleTxt = new System.Windows.Forms.TextBox();
+            this.distanceTxt = new System.Windows.Forms.TextBox();
+            this.daysTxt = new System.Windows.Forms.TextBox();
+            this.calculateCostButton = new System.Windows.Forms.Button();
+            this.decisionOutputLbl = new System.Windows.Forms.Label();
+            this.resultsBox = new System.Windows.Forms.GroupBox();
+            this.resultsBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // peopleLbl
+            // 
+            this.peopleLbl.AutoSize = true;
+            this.peopleLbl.Location = new System.Drawing.Point(89, 83);
+            this.peopleLbl.Name = "peopleLbl";
+            this.peopleLbl.Size = new System.Drawing.Size(40, 13);
+            this.peopleLbl.TabIndex = 0;
+            this.peopleLbl.Text = "People";
+            // 
+            // distanceLbl
+            // 
+            this.distanceLbl.AutoSize = true;
+            this.distanceLbl.Location = new System.Drawing.Point(89, 132);
+            this.distanceLbl.Name = "distanceLbl";
+            this.distanceLbl.Size = new System.Drawing.Size(82, 13);
+            this.distanceLbl.TabIndex = 1;
+            this.distanceLbl.Text = "Distance (Miles)";
+            // 
+            // daysLbl
+            // 
+            this.daysLbl.AutoSize = true;
+            this.daysLbl.Location = new System.Drawing.Point(89, 183);
+            this.daysLbl.Name = "daysLbl";
+            this.daysLbl.Size = new System.Drawing.Size(72, 13);
+            this.daysLbl.TabIndex = 2;
+            this.daysLbl.Text = "Delivery Days";
+            // 
+            // companyALbl
+            // 
+            this.companyALbl.AutoSize = true;
+            this.companyALbl.Location = new System.Drawing.Point(49, 56);
+            this.companyALbl.Name = "companyALbl";
+            this.companyALbl.Size = new System.Drawing.Size(85, 13);
+            this.companyALbl.TabIndex = 3;
+            this.companyALbl.Text = "Company A Cost";
+            // 
+            // companyAOutputLbl
+            // 
+            this.companyAOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.companyAOutputLbl.Location = new System.Drawing.Point(156, 51);
+            this.companyAOutputLbl.Name = "companyAOutputLbl";
+            this.companyAOutputLbl.Size = new System.Drawing.Size(86, 23);
+            this.companyAOutputLbl.TabIndex = 4;
+            this.companyAOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // companyBOutputLbl
+            // 
+            this.companyBOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.companyBOutputLbl.Location = new System.Drawing.Point(156, 100);
+            this.companyBOutputLbl.Name = "companyBOutputLbl";
+            this.companyBOutputLbl.Size = new System.Drawing.Size(86, 23);
+            this.companyBOutputLbl.TabIndex = 6;
+            this.companyBOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // companyBLbl
+            // 
+            this.companyBLbl.AutoSize = true;
+            this.companyBLbl.Location = new System.Drawing.Point(49, 105);
+            this.companyBLbl.Name = "companyBLbl";
+            this.companyBLbl.Size = new System.Drawing.Size(85, 13);
+            this.companyBLbl.TabIndex = 5;
+            this.companyBLbl.Text = "Company B Cost";
+            // 
+            // companyCOutputLbl
+            // 
+            this.companyCOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.companyCOutputLbl.Location = new System.Drawing.Point(156, 151);
+            this.companyCOutputLbl.Name = "companyCOutputLbl";
+            this.companyCOutputLbl.Size = new System.Drawing.Size(86, 23);
+            this.companyCOutputLbl.TabIndex = 8;
+            this.companyCOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // companyCLbl
+            // 
+            this.companyCLbl.AutoSize = true;
+            this.companyCLbl.Location = new System.Drawing.Point(49, 156);
+            this.companyCLbl.Name = "companyCLbl";
+            this.companyCLbl.Size = new System.Drawing.Size(85, 13);
+            this.companyCLbl.TabIndex = 7;
+            this.companyCLbl.Text = "Company C Cost";
+            // 
+            // peopleTxt
+            // 
+            this.peopleTxt.Location = new System.Drawing.Point(192, 80);
+            this.peopleTxt.Name = "peopleTxt";
+            this.peopleTxt.Size = new System.Drawing.Size(100, 20);
+            this.peopleTxt.TabIndex = 9;
+            // 
+            // distanceTxt
+            // 
+            this.distanceTxt.Location = new System.Drawing.Point(192, 129);
+            this.distanceTxt.Name = "distanceTxt";
+            this.distanceTxt.Size = new System.Drawing.Size(100, 20);
+            this.distanceTxt.TabIndex = 10;
+            // 
+            // daysTxt
+            // 
+            this.daysTxt.Location = new System.Drawing.Point(192, 180);
+            this.daysTxt.Name = "daysTxt";
+            this.daysTxt.Size = new System.Drawing.Size(100, 20);
+            this.daysTxt.TabIndex = 11;
+            // 
+            // calculateCostButton
+            // 
+            this.calculateCostButton.Location = new System.Drawing.Point(103, 262);
+            this.calculateCostButton.Name = "calculateCostButton";
+            this.calculateCostButton.Size = new System.Drawing.Size(107, 23);
+            this.calculateCostButton.TabIndex = 12;
+            this.calculateCostButton.Text = "Calculate Cost";
+            this.calculateCostButton.UseVisualStyleBackColor = true;
+            this.calculateCostButton.Click += new System.EventHandler(this.CalculateCostButton_Click);
+            // 
+            // decisionOutputLbl
+            // 
+            this.decisionOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.decisionOutputLbl.Location = new System.Drawing.Point(52, 192);
+            this.decisionOutputLbl.Name = "decisionOutputLbl";
+            this.decisionOutputLbl.Size = new System.Drawing.Size(190, 21);
+            this.decisionOutputLbl.TabIndex = 13;
+            this.decisionOutputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.Controls.Add(this.decisionOutputLbl);
+            this.resultsBox.Controls.Add(this.companyALbl);
+            this.resultsBox.Controls.Add(this.companyAOutputLbl);
+            this.resultsBox.Controls.Add(this.companyBLbl);
+            this.resultsBox.Controls.Add(this.companyBOutputLbl);
+            this.resultsBox.Controls.Add(this.companyCLbl);
+            this.resultsBox.Controls.Add(this.companyCOutputLbl);
+            this.resultsBox.Location = new System.Drawing.Point(362, 24);
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.Size = new System.Drawing.Size(294, 280);
+            this.resultsBox.TabIndex = 15;
+            this.resultsBox.TabStop = false;
+            this.resultsBox.Text = "Results";
+            // 
+            // calculatorForm
+            // 
+            this.AcceptButton = this.calculateCostButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 373);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.calculateCostButton);
+            this.Controls.Add(this.daysTxt);
+            this.Controls.Add(this.distanceTxt);
+            this.Controls.Add(this.peopleTxt);
+            this.Controls.Add(this.daysLbl);
+            this.Controls.Add(this.distanceLbl);
+            this.Controls.Add(this.peopleLbl);
+            this.Name = "calculatorForm";
+            this.Text = "Food Delivery Cost Calculation";
+            this.resultsBox.ResumeLayout(false);
+            this.resultsBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label peopleLbl;
+        private System.Windows.Forms.Label distanceLbl;
+        private System.Windows.Forms.Label daysLbl;
+        private System.Windows.Forms.Label companyALbl;
+        private System.Windows.Forms.Label companyAOutputLbl;
+        private System.Windows.Forms.Label companyBOutputLbl;
+        private System.Windows.Forms.Label companyBLbl;
+        private System.Windows.Forms.Label companyCOutputLbl;
+        private System.Windows.Forms.Label companyCLbl;
+        private System.Windows.Forms.TextBox peopleTxt;
+        private System.Windows.Forms.TextBox distanceTxt;
+        private System.Windows.Forms.TextBox daysTxt;
+        private System.Windows.Forms.Button calculateCostButton;
+        private System.Windows.Forms.Label decisionOutputLbl;
+        private System.Windows.Forms.GroupBox resultsBox;
+    }
+}
+
---
+++ Program2/Form1.cs
@@ -0,0 +1,160 @@
+﻿//Grading ID: R5560
+//Program 2
+//Due Date: March 11, 2021
+//Course Section CIS 199-01
+/*Description: Depending on the entered data by the user, the program tells the user which food delivery
+cost the least for their event */
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class calculatorForm : Form
+    {
+        private double costA; //variable used to hold cost of company A
+        private double costB; //variable used to hold cost of company B
+        private double costC; //variable used to hold cost of company C
+        public calculatorForm()
+        {
+            InitializeComponent();
+        }
+        //event handler that runs whenever the Calculate cost button is clicked. Calcuates the cost of each
+        //company based on the data entered by the user. 
+        private void CalculateCostButton_Click(object sender, EventArgs e)
+        {
+            int people, //entered by user. How many people are at event
+                days, //entered by user. How many days for delivery
+                peopleACost, //used to calculate the cost for the amount of people via company A
+                daysACost, //used to calculate the cost for how many delivery days via company A
+                daysBCost, //used to calculate the cost for how many delivery days via company B
+                daysCCost; //used to calculate the cost for how many delivery days via company C
+            double distance, //entered by user. How many miles is the delivery
+                distanceACost, //used to calculate the cost for the distance in miles via company A
+                distanceBCost, //used to calculate the cost for the distance in miles via company B
+                distanceCCost, //used to calculate the cost for the distance in miles via company C
+                peopleBCost, //used to calculate the cost for the amount of people via company B
+                peopleCCost; //used to calculate the cost for the amount of people via company C
+
+            //Tryparse to determine if entered data is acceptable.
+            //intergers must be intergers, doubles must be doubles, and all inputs can not be negative
+            //if so, runs the code to determine the prices of each variable and total cost for each company
+            if (int.TryParse(peopleTxt.Text, out people) && (int.TryParse(daysTxt.Text, out days)) && (double.TryParse(distanceTxt.Text, out distance)) && (people >= 0) && (days >= 0) && (distance >= 0))
+            {
+                //starts finding price for Company A
+                //price for people
+                peopleACost = people * 1;
+                //price for distance
+                distanceACost = distance * .02;
+                switch(days) //used switch here to determine price of delivery days for company A, could have used else if statements
+                {
+                    case 1: //if days equals 1
+                        daysACost = 20;
+                        break;
+                    case 2: //2
+                        daysACost = 17;
+                        break;
+                    case 3: //3
+                        daysACost = 15;
+                        break;
+                    case 4: //etc
+                        daysACost = 10;
+                        break;
+                    case 5:
+                        daysACost = 10;
+                        break;
+                    case 6:
+                        daysACost = 10;
+                        break;
+                    case 7:
+                        daysACost = 10;
+                        break;
+                    default: //if not any of the previous
+                        daysACost = 7;
+                        break;
+                }
+
+                costA = peopleACost + distanceACost + daysACost; //adds prices of all other variable to get total cost for company A
+                companyAOutputLbl.Text = $"${costA:F2}"; //outputs it into the corresponding label with 2 units of precision
+                
+                //starts finding price for company B
+                //price for people
+                //if statements for a range of numbers to determine price depending on the amount of people
+                if (people < 10)
+                    peopleBCost = people * 20;
+                else if (people >= 10 && people < 50)
+                    peopleBCost = people * 10;
+                else if (people >= 50 && people < 100)
+                    peopleBCost = people * 5;
+                else if (people >= 100 && people < 200)
+                    peopleBCost = people *3;
+                else
+                    peopleBCost = people *.15;
+
+                //price for distance
+                distanceBCost = distance * .10;
+                
+                //price for days
+                if (days >= 1 && days <= 4) //if days is between 1 and 4 inclusive
+                    daysBCost = 10;
+                else //anything else
+                    daysBCost = 7;
+
+                costB = peopleBCost + distanceBCost + daysBCost; //totals costs for company B
+                companyBOutputLbl.Text = $"${costB:F2}"; //output with 2 units of precision
+
+                //starts finding price for company C
+                //price for people
+                peopleCCost = people * .25;
+                //price for distance
+                if (distance >= 1000)
+                    distanceCCost = 40;
+                else if (distance >= 750 && distance < 1000)
+                    distanceCCost = 35;
+                else if (distance >= 500 && distance < 750)
+                    distanceCCost = 25;
+                else if (distance >= 200 && distance < 500)
+                    distanceCCost = 15;
+                else
+                    distanceCCost = 10;
+                //price for days
+                daysCCost = 20;
+
+                costC = peopleCCost + distanceCCost + daysCCost; //total costs company C
+                companyCOutputLbl.Text = $"${costC:F2}"; //output with 2 units of precision
+
+                //if statements to determine which company costs the least and output that information to the user
+                if (costA < costB & costA < costC) //if cost for company a is less than other 2
+                    decisionOutputLbl.Text = $"The lowest cost company is: A";
+                else if(costB < costA & costB < costC) // if cost for compnay b is less than other 2
+                    decisionOutputLbl.Text = $"The lowest cost company is: B";
+                else //if cost of c is less than other 2
+                    decisionOutputLbl.Text = $"The lowest cost company is: C";
+            }
+            else //happens if the original tryparse and conditions are not met
+            {
+                //Message boxes that show if the data the user inputted is not accepted. IE: Not an integer or is a negative number
+                if (int.TryParse(peopleTxt.Text, out people) == false || people <= 0) //if people input was not an int or was negative
+                {
+                    MessageBox.Show("Invalid Number of People"); //output in a message box
+                }
+                else if (int.TryParse(daysTxt.Text, out days) == false || (days < 0)) //if days input was not an integer or it was negative
+                {
+                    MessageBox.Show("Invalid Delivery Days"); //output message box
+                }
+                else if (double.TryParse(distanceTxt.Text, out distance) == false || (distance < 0)) //double is allowed to be a decimal, so if it was negaitve
+                {
+                    MessageBox.Show("Invalid Distance"); //output
+                }
+
+            }
+        }   
+    }
+}
